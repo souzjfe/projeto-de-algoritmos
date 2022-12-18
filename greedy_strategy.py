09ea5322a66c85abd74117dc07f4e5bd3c52b6e1
Jeferson Souza <souzajefison@gmail.com>
2022-12-18 18:26:41 -0300

    chore: up - 🔧

--- greedy_strategy.py
+++ greedy_strategy.py
@@ -1,14 +1,17 @@
-from utils import file_reader, greedy_TSP
+from utils import file_reader, greedy_TSP, two_opt, write_table
 
 if __name__ == '__main__':
-    files = ['TSPLIB/a280.tsp', 'TSPLIB/ali535.tsp', 'TSPLIB/att532.tsp', 'TSPLIB/berlin52.tsp', 'TSPLIB/bier127.tsp', 'TSPLIB/burma14.tsp', 'TSPLIB/d198.tsp', 'TSPLIB/fl417.tsp', 'TSPLIB/rat575.tsp']
-    string = 'NOME DO ARQUIVO  DISTANCIA  TEMPO  ROTA\n'
+    best_solutions = [2579, 27686, 7542, 118282, 15780, 11861, 6773]
+    files = ['TSPLIB/a280.tsp',  'TSPLIB/att532.tsp', 'TSPLIB/berlin52.tsp', 'TSPLIB/bier127.tsp', 'TSPLIB/d198.tsp', 'TSPLIB/fl417.tsp', 'TSPLIB/rat575.tsp']
+    two_opt_solutions = list()
+    greedy_solutions = list()
+    graph_time = list()
     for file in files:
         graph = file_reader(file)
-        solution = greedy_TSP(graph)
-        string += f'{file}  {solution[0]}  {solution[1]}\n'
-    with open('output.txt', 'w') as file:
-        file.write(string)
+        greedy_solution = greedy_TSP(graph)
+        greedy_solutions.append(greedy_solution[0])
+        two_opt_solutions.append(two_opt(graph, greedy_solution[2])[1])
+    write_table(graph_name = files, best_solutions = best_solutions, greedy_solutions = greedy_solutions, tow_opt__solutions = two_opt_solutions, name = 'greedy_strategy.md', title='Greedy Strategy')
 
 
 
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 from cmath import inf, sqrt
+import random
+from this import d
 from time import process_time
 from math import inf
-
+import mdutils
 
 def file_reader(path : str):
     with open(path, 'r') as file:
@@ -59,6 +61,21 @@ def upperrow_graph(file : list, graph_start : int):
             index += 1
 
     return tuple(graph)
+# Função provida pelo Luiz
+def write_table(graph_name : list, best_solutions : list, greedy_solutions : list, tow_opt__solutions : list, name : str = 'Output', title : str = 'Title'):
+
+    file = mdutils.MdUtils(file_name=name, title=title)
+    table_content = ['Grafo', 'Melhor solução conhecida', 'Solução Gulosa','Solução pela 2 OPT']
+
+    for g_name, g_best_solution, g_g, g_2 in zip(graph_name, best_solutions, greedy_solutions, tow_opt__solutions):
+        table_content.append(g_name)
+        table_content.append(g_best_solution)
+        table_content.append(g_g)
+        table_content.append(g_2)
+
+    file.new_table(4, len(graph_name) + 1, text = table_content, text_align='center')
+
+    file.create_md_file()
 
 def greedy_TSP(graph):
     traveled = [0]
@@ -67,15 +84,13 @@ def greedy_TSP(graph):
     begin = process_time()
 
     while(len(traveled) != len(graph)):
-        min_distance = min_index = inf
+        min_distance = inf
+        min_index = inf
 
         for index in range(len(graph)):
-            if index in traveled:
-                pass
-            else:
-                if graph[current][index] < min_distance:
-                    min_distance = graph[current][index]
-                    min_index = index
+            if not index in traveled and graph[current][index] < min_distance and round(graph[current][index], 2) > 0 and round(current, 2) != index:
+                min_distance = graph[current][index]
+                min_index = index
         current = min_index
         traveled.append(current)
         distance += min_distance
@@ -83,9 +98,25 @@ def greedy_TSP(graph):
     
     return distance, process_time() - begin, traveled
 
+def two_opt(graph, traveled):
+    best_solution = traveled
+    best_distance = distance_verify(graph, traveled)
+    for i in range(len(traveled)):
+        for j in range(i + 1, len(traveled)):
+            new_traveled = traveled.copy()
+            new_traveled[i:j] = reversed(new_traveled[i:j])
+            new_distance = distance_verify(graph, new_traveled)
+            if new_distance < best_distance:
+                best_distance = new_distance
+                best_solution = new_traveled
+    return best_solution, best_distance
 
+def distance_verify(graph, traveled):
+    distance = 0
+    for i in range(len(traveled) - 1):
+        distance += graph[traveled[i]][traveled[i + 1]]
+    distance += graph[traveled[-1]][traveled[0]]
+    return distance
 if __name__ == '__main__':
     files = ['TSPLIB/a280.tsp', 'TSPLIB/ali535.tsp', 'TSPLIB/att532.tsp', 'TSPLIB/berlin52.tsp', 'TSPLIB/bier127.tsp', 'TSPLIB/brd14051.tsp', 'TSPLIB/burma14.tsp', 'TSPLIB/d198.tsp', 'TSPLIB/fl417.tsp', 'TSPLIB/rat575.tsp']
     
-    for file in files:
-        print(file_reader(file))
